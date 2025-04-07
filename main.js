8a4ff37c37e960d68e455e9ca5283a9a462b1ec0
maycon345 <146994229+maycon345@users.noreply.github.com>
2025-04-07 08:28:13 -0300

    .

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const botoes = document . querySelectorAll(".botao");
 console . log(botoes);
 for (let i = 0; i < botoes.length; i++) {
     console  . log(i);
+    for(let j=0;j<botoes.length;j++){
+        botoes[j].classList.remove("ativo");
+    }
+
     
 }
 botoes[i]. onclick = function () {
